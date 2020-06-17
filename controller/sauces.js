d823bce3bc200f8ef02e6d47cb2420f84882cca5
mariammartinez <mariammartinez@users.noreply.github.com>
2020-06-17 20:19:39 +0200

    like ajoutées

--- controller/sauces.js
+++ controller/sauces.js
@@ -27,22 +27,69 @@ exports.createSauce = (req, res, next) => {
 };
 
 exports.createLike = (req, res, next) =>{
+
   Sauce.findOne({ _id: req.params.id })
+  .then( sauce => {
+
+    // si user like
+    if(req.body.like === 1){
+      // check si pas déjà liké
+      if(sauce.usersLiked.indexOf(req.body.userId) !== -1){
+        // envoi dans catch ?
+      }else{
+        sauce.likes++;
+        sauce.usersLiked.push(req.body.userId);
+      }
+    }
+    // si user annule like ou dislike
+    if(req.body.like === 0){
+      // check si user a déjà liké
+      if(sauce.usersLiked.indexOf(req.body.userId) !== -1){
+        sauce.likes--;
+        let i = sauce.usersLiked.indexOf(req.body.userId);
+        sauce.usersLiked.splice(i, 1);
+      }
+      // check si user a déjà disliké
+      if(sauce.usersDisliked.indexOf(req.body.userId) !== -1){
+        sauce.dislikes--;
+        let i = sauce.usersDisliked.indexOf(req.body.userId);
+        sauce.usersDisliked.splice(i, 1);
+      }
+    }
+    // si user dislike
+    if(req.body.like === -1){
+      // check si pas déjà disliké      
+      if(sauce.usersDisliked.indexOf(req.body.userId) !== -1){
+        // envoi dans catc  h ?
+      }else{
+        sauce.dislikes++;
+        sauce.usersDisliked.push(req.body.userId);
+      }
+    }
+
+    sauce.save()
+    .then(res.status(201).json({message: 'objet liké'}))
+  })
+
+
+     /*
   .then(() => res.status(201).json({message: 'objet liké'}))
   .catch(error => res.status(404).json({ error }));
-  }
-
+  console.log(sauce)
+*/
+ 
+}
 
 exports.modifySauce = (req, res, next) => {
   const sauceObject = req.file ?
     {
       ...JSON.parse(req.body.sauce),
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+      imageUrl: `${req.proto+col}://${req.get('host')}/images/${req.file.filename}`
     } : { ...req.body };
     Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Objet modifié !'}))
         .catch(error => res.status(400).json({ error }));
-    };
+};
 
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
